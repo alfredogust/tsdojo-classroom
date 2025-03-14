2debcd9845eb7086602ab014b8dfff3e950bd2ba
alfredogust <alfredog.fu@gmail.com>
2025-03-14 10:24:12 -0300

    feat: add function to display informed number
    - Added function to ask the user to input a number.
    - Validates if the input is a number and displays a message accordingly.
    - Handles invalid input by notifying the user.

--- src/modules/basic-operations/basic-operations.ts
+++ src/modules/basic-operations/basic-operations.ts
@@ -3,3 +3,13 @@ import * as readlineSync from 'readline-sync';
 export const helloWorld = (): void => {
     console.log("Hello, World!");
 };
+
+export const displayInformedNumber = (): void => {
+    const informedNumber: number = Number(readlineSync.question("Enter a number: "));
+
+    if (isNaN(informedNumber)) {
+        console.log("That is not a valid number!");
+    } else {
+        console.log(`The informed number was ${informedNumber}`);
+    }
+}   
